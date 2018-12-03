15d0d44e2e7fcce9c40c4306b0f21d9d6d7c5b7f
Oscar Sandoval <apollo.sandoval@gmail.com>
2018-12-03 16:51:46 -0700

    dynamically generating cart items

---
+++ src/components/CartItems.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+const CartItems = ({items}) => {
+    const cartItems = items.map((item) => {
+        return <CartItem item={item} />
+    });
+
+    return (
+        <div className="container">
+            <h1>Items</h1>
+            <div className="list-group">
+                <div className="list-group-item">
+                    <div className="row">
+                        <div className="col-md-8">Product</div>
+                        <div className="col-md-2">Price</div>
+                        <div className="col-md-2">Quantity</div>
+                    </div>
+                </div>
+                {cartItems}
+            </div>
+        </div>
+    )
+}
+
+export default CartItems;
